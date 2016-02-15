2b7823b207c1fef8281bccaecd94e66c516b45fd
KaPJlCooH <mu4work@gmail.com>
2016-02-15 18:28:20 +0700

    #36 Оживить интерактивные элементы в личном проекте (0.2)
    Поправил footer, вставил карту с помощью JS

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ openClose.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.toggle("booking-form-show");
 });
+
+
+function initialize() {
+  var mapOptions = {
+    zoom: 10,
+    center: new google.maps.LatLng(34.8390006,-111.840093)
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+                                mapOptions);
+  var image = "img/map-logo.png";
+  var myLatLng = new google.maps.LatLng(34.877,-111.762);
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    icon: image
+  });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
